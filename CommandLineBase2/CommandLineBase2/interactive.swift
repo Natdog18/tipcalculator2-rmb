4bbc42688fdea2c2319f4b31dc0dd94595762c3a
Natdog18 <rosebailey@Kimberlys-MacBook-Pro-4.local>
2016-10-11 09:30:49 -0400

    final

--- CommandLineBase2/CommandLineBase2/interactive.swift
+++ CommandLineBase2/CommandLineBase2/interactive.swift
@@ -11,6 +11,11 @@ class Interactive{
     private var done: Bool = false
     private var currentInPut: String = "q"
     private var io = Io()
+    //var mealCost = ""
+    //var tipPercentage = ""
+    //var totalTip: Float = 0.0
+    
+    
     func go(){
         
         while !done {
@@ -21,12 +26,46 @@ class Interactive{
             
             if currentInPut == "q" {
                 done = true
-            }else {
-                print("The input is: \(currentInPut)")
+                
+            } else if currentInPut == "t" {
+                 calculateTip()
+        
+            
+            } else {
+                print("Inter a valid choice")
             }
         }
         print ("Exiting....")
         return
     }
+    func calculateTip() {
+        
+                io.writeMessage("\n How much was the bill?")
+            currentInPut = io.getInput()
+
+        let billAmount: Double? = Double(currentInPut)
+        
+        io.writeMessage("\n What is percent of your tip?")
+        currentInPut = io.getInput()
+
+        let tipPercent: Double? = Double(currentInPut)
+        
+        let totalTip = ((tipPercent ?? 0.0) / 100)
+       
+        let totalTipAmount = ((billAmount ?? 0.0) * totalTip)
+        print("amount of tip is: \(String(format:"%.02f", round(num:totalTipAmount)))")
+        
+        let finalBill = ((billAmount ?? 0.0) + totalTipAmount)
+        
+        print("amount of total bill: \(String(format:"%.02f", round(num:finalBill)))")
+        
+        
+    }
     
+        func round(num: Double) -> Double {
+            let tmp = (num*100).rounded(.toNearestOrAwayFromZero)
+            return tmp/100
+        }
 }
+
+        
